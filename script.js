1d502f13f6931cd016176d5295c754f0e85a59a0
Giulio_Pa <giuliop93@hotmail.it>
2021-04-18 20:14:43 +0200

    non funziona :(

---
+++ script.js
@@ -0,0 +1,7 @@
+// Collegare onclick su bottone con semplice console.log("ciao");
+function myfunction(){
+    document.getElementById("data").innerHTML = "test";
+}
+
+// 1 Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
