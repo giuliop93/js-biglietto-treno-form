e0d2ed229a638ba442cb29c3e83cdd71e96dbc97
Giulio_Pa <giuliop93@hotmail.it>
2021-04-19 15:34:09 +0200

    wip

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Collegare onclick su bottone con semplice console.log("ciao");
-function myfunction(){
+
+// Sull'evento onclick prima collegato, ora mettere nel console.log i dati presi dall'input utente
+var bottoneGenera = document.getElementById("genera");
+
+bottoneGenera.addEventListener("click", function(event) {
     document.getElementById("data").value = "test";
     console.log("funziona")
-}
+});
 
 // 1 Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
