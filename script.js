ce31db3d1d457dbf65f1ea2ccb87c09f07fd3528
Giulio_Pa <giuliop93@hotmail.it>
2021-04-27 18:05:59 +0200

    wip

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-
 // Sull'evento onclick prima collegato, ora mettere nel console.log i dati presi dall'input utente
 var bottoneGenera = document.getElementById("genera");
 
-bottoneGenera.addEventListener("click", function(event) {
-    document.getElementById("data").value = "";
+bottoneGenera.addEventListener("click", function (event) {
+    event.preventDefault();
     var nomePasseggero = ""
-    console.log(nomePasseggero)
+    console.log(nomePasseggero);
+    // 1 Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+    // 2 Il prezzo del biglietto è definito in base ai km (0.21 € al km),
+    // va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+    // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+    var passeggero = document.getElementById("dataPassenger").value;
+    console.log(passeggero);
+    var percorrenza = document.getElementById("kilometers").value;
+    var eta = document.getElementById("ageRange");
+
+    var offerta = null;
+    var numeroCarrozza = null;
+    var codiceTreno = null;
+    var prezzoAlKilometro = 0.21;
+    var prezzoBigliettoSenzaSconto = prezzoAlKilometro * percorrenza;
+    var costoBiglietto = null;
+
+    var scritto = "Il prezzo del biglietto è "
+    var euro =
+        console.log(prezzoBigliettoSenzaSconto);
+
+
+    if (eta <= 18) {
+        document.getElementById('costo').innerHTML = "Il prezzo del biglietto è " + ((prezzoBigliettoSenzaSconto / 100) * 80) + " €";
+    } else if (eta >= 65) {
+        document.getElementById('costo').innerHTML = scritto + ((prezzoBigliettoSenzaSconto / 100) * 60) + euro;
+    } else {
+        document.getElementById('costo').innerHTML = scritto + (prezzoBigliettoSenzaSconto) + euro;
+    }
 });
 
-// 1 Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
